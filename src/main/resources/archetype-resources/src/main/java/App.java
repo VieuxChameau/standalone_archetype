d79cf4aeca892b76d548361fb4d1a5f5fe3776d1
VieuxChameau <VieuxChameau@users.noreply.github.com>
2016-01-12 13:56:40 +0000

    Add generation of the package based on the groupId, add mockito to the pom

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,7 @@
+package ${package};
+
+public class App {
+    public static void main(final String[] args) {
+
+    }
+}
---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,19 @@
+package ${package};
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class AppTest {
+
+}
